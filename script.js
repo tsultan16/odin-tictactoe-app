eea2843bc8e182dbbecfb9ff471a9e517fd55248
tsultan16 <tanzids@student.unimelb.edu.au>
2025-01-22 23:18:10 +1100

    implemented game restart button functionality

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ function createPlayer(name, type) {
     return {getName, getType}
 }
 
-function startGame() {
+
+function startGame(gameBoard) {
 
     // get player names (hard-coded for now)
     const name1 = "Alice"; 
@@ -19,85 +20,12 @@ function startGame() {
     console.log(`Player 2 name: ${player2.getName()}, type: ${player2.getType()}`); 
 
 
-    // create game-board
-    const gameBoard = (function () {
-        // create empty game grid, will make it private
-        let grid = [];
-        for (let i = 0; i < 9; i++){
-            grid.push("-");
-        } 
-
-        const display = () => {
-            console.log(`\n${grid[0]} | ${grid[1]} | ${grid[2]}\n${grid[3]} | ${grid[4]} | ${grid[5]}\n${grid[6]} | ${grid[7]} | ${grid[8]}\n`);
-        };
-
-        const play = (player, position) => {
-            if (grid[position] === "-"){
-                grid[position] = player.getType();
-                return true;
-            } else {
-                console.log("Invalid move. Board position already filled!");
-                return false;
-            }
-        };
-
-        const checkGameWin = () => {
-            // check for win pattern (same symbol along any column/row/diagonal)
-
-            // check rows
-
-            if (grid[0] === grid[1] && grid[0] === grid[2] && !(grid[0]+grid[1]+grid[2]).includes("-")) {
-                return [0,1,2];
-            }    
-            if (grid[3] === grid[4] && grid[3] === grid[5] && !(grid[3]+grid[4]+grid[5]).includes("-")) {
-                return [3,4,5];
-            }    
-            if (grid[6] === grid[7] && grid[6] === grid[8] && !(grid[6]+grid[7]+grid[8]).includes("-")) {
-                return [6,7,8];
-            }  
-
-            // check columns
-            if (grid[0] === grid[3] && grid[0] === grid[6] && !(grid[0]+grid[3]+grid[6]).includes("-")) {
-                return [0,3,6];
-            } 
-            if (grid[1] === grid[4] && grid[1] === grid[7] && !(grid[1]+grid[4]+grid[7]).includes("-")) {
-                return [1,4,7];
-            } 
-            if (grid[2] === grid[5] && grid[2] === grid[8] && !(grid[2]+grid[5]+grid[8]).includes("-")) {
-                return [2,5,8];
-            } 
-         
-            // check diagonals
-            if (grid[0] === grid[4] && grid[0] === grid[8] && !(grid[0]+grid[4]+grid[8]).includes("-")) {
-                return [0,4,8];
-            } 
-            if (grid[2] === grid[4] && grid[2] === grid[6] && !(grid[2]+grid[4]+grid[6]).includes("-")) {
-                return [2,4,6];
-            } 
-
-            return false;
-
-        };
-
-
-        return {display, play, checkGameWin}
-
-    }) (); 
-
     console.log("game board: ", gameBoard);
     gameBoard.display();
 
     const playRound = (position) => {
-        // current_player = (current_turn%2 == 0) ? player1 : player2;
-        //let done = false;
-        // loop until user gives valid input position
-        //do {
-        //    const position = window.prompt(`${current_player.getName()} choose your move position.`);
         gameBoard.play(current_player, position);
-        //} while(!done);
-
         gameBoard.display();
-        // return current_player;
     };
 
     const symbolConvert = (type) => {
@@ -109,22 +37,6 @@ function startGame() {
                 return "cross";
         }
     };
-
-    // let gameOver = false;
-    // while(!gameOver){
-    //     const p = playRound();
-    //     const won = gameBoard.checkGameWin();
-    //     if (won) {
-    //         gameOver = true;
-    //         console.log(`${p.getName()} has won the game!`);
-    //     };
-
-    //     // check if all slots filled
-    //     if (current_turn === 9) {
-    //         gameOver = true;
-    //         console.log(`And it's a draw!`);
-    //     }
-    // }
 
     // initialize current turn number of current player
     let current_turn = 0;
@@ -161,24 +73,23 @@ function startGame() {
                 
 
             };
-    
+            current_turn++;
+            
             // check if all slots filled
             if (current_turn === 9) {
                 gameOver = true;
                 console.log(`And it's a draw!`);
                 player_turn.textContent = `Game over. It's a draw!`;
             }
-
+            
             if(!gameOver) {
                 // pass the turn to other player
-                current_turn++;
-                current_player = (current_turn%2 == 0) ? player1 : player2;
+                current_player = (current_turn%2 === 0) ? player1 : player2;
                 player_turn.textContent =  `${current_player.getName()} make your move`;
             } else {
                 // give option to restart the game... maybe with modal dialog form
             } 
-            
-        
+                    
         } else {
             console.log("Invalid move. Board position already filled!");
         }
@@ -192,8 +103,94 @@ function startGame() {
 
 const container = document.querySelector(".container");
 const player_turn = document.querySelector(".player-turn");
+const restart = document.querySelector(".restart-btn");
 
+
+// create game-board
+const gameBoard = (function () {
+    // create empty game grid, will make it private
+    let grid = [];
+    for (let i = 0; i < 9; i++){
+        grid.push("-");
+    } 
+
+    const display = () => {
+        console.log(`\n${grid[0]} | ${grid[1]} | ${grid[2]}\n${grid[3]} | ${grid[4]} | ${grid[5]}\n${grid[6]} | ${grid[7]} | ${grid[8]}\n`);
+    };
+
+    const play = (player, position) => {
+        if (grid[position] === "-"){
+            grid[position] = player.getType();
+            return true;
+        } else {
+            console.log("Invalid move. Board position already filled!");
+            return false;function eraseGrid() {
+            }
+        }
+    };
+
+    const checkGameWin = () => {
+        // check for win pattern (same symbol along any column/row/diagonal)
+
+        // check rows
+
+        if (grid[0] === grid[1] && grid[0] === grid[2] && !(grid[0]+grid[1]+grid[2]).includes("-")) {
+            return [0,1,2];
+        }    
+        if (grid[3] === grid[4] && grid[3] === grid[5] && !(grid[3]+grid[4]+grid[5]).includes("-")) {
+            return [3,4,5];
+        }    
+        if (grid[6] === grid[7] && grid[6] === grid[8] && !(grid[6]+grid[7]+grid[8]).includes("-")) {
+            return [6,7,8];
+        }  
+
+        // check columns
+        if (grid[0] === grid[3] && grid[0] === grid[6] && !(grid[0]+grid[3]+grid[6]).includes("-")) {
+            return [0,3,6];
+        } 
+        if (grid[1] === grid[4] && grid[1] === grid[7] && !(grid[1]+grid[4]+grid[7]).includes("-")) {
+            return [1,4,7];
+        } 
+        if (grid[2] === grid[5] && grid[2] === grid[8] && !(grid[2]+grid[5]+grid[8]).includes("-")) {
+            return [2,5,8];
+        } 
+        
+        // check diagonals
+        if (grid[0] === grid[4] && grid[0] === grid[8] && !(grid[0]+grid[4]+grid[8]).includes("-")) {
+            return [0,4,8];
+        } 
+        if (grid[2] === grid[4] && grid[2] === grid[6] && !(grid[2]+grid[4]+grid[6]).includes("-")) {
+            return [2,4,6];
+        } 
+
+        return false;
+    };
+
+    const eraseGrid = () => {
+        // reset grid array and remove html class for the cross/circle images
+        for (let i = 0; i < 9; i++){
+            grid[i] = "-";
+            const cell = document.querySelector(`[id='${i}']`);
+            cell.classList.remove("cross", "circle", "win");
+        } 
+        console.log("Grid erased!");
+        display();
+    };
+
+    return {display, play, checkGameWin, eraseGrid}
+
+}) (); 
+
+
+
+// add event listener for triggering game restart
+restart.addEventListener("click", (e) => {
+    // erase the board and restart game
+    gameBoard.eraseGrid();
+    startGame(gameBoard);
+    console.log("Game restarted!");
+});
 
 
 console.log("Begin game.")
-startGame();
+startGame(gameBoard);
